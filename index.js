143cd55ab2389ef6348c3e10c7197598233d7448
yaser eftekhari <yeftekhari@gmail.com>
2017-05-25 15:54:36 +0430

    Basic CSS and HTML integrations for the plugin!

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 // var UiContainerPlugin = require('UIContainerPlugin');
 var JST = require('./jst');
+var css = require('css');
 
 class OverlayButtons extends Clappr.UIContainerPlugin {
   get name() {
     return 'overlay_buttons'
   }
 
-  render() {
-    console.log("rendering", this.name)
-    var style = $('<style>').html(JST.CSS[this.name])
-    this.$el.append(style)
-    return this;
+  initialize() {
+    this.render();
   }
 
+  bindEvents() {
+    this.listenTo(this.container, Clappr.Events.CONTAINER_PAUSE, this.show);
+    this.listenTo(this.container, Clappr.Events.CONTAINER_PLAY, this.hide);
+    this.listenTo(this.container, Clappr.Events.CONTAINER_READY, this.render)
+  }
+
+  hide() {
+    this.$el.hide();
+  }
+
+  show() {
+    this.$el.show();
+  }
+
+  render() {
+    this.$el.html(JST.overlay_buttons());
+    let $el = this.$el;
+    css.parse(JST.CSS.overlay_buttons, {compress: true}).stylesheet.rules.forEach(function (rule) {
+      let elem = $el.find("div.clappr-overlay-container");
+      rule.declarations.forEach(function (declaration) {
+        elem.css(declaration.property, declaration.value);
+      });
+    });
+    // this.$el.css('color', 'white');
+    // this.$el.css('background-color', 'red');
+    // this.$el.css('position', 'relative');
+    this.container.$el.append(this.$el);
+    this.hide();
+    return this;
+  }
 }
 
 module.exports = window.OverlayButtons = OverlayButtons;
--- jst.js
+++ jst.js
@@ -2,7 +2,11 @@
 var template = require('lodash.template');
 module.exports = {
 
+  'overlay_buttons': template('<div class="clappr-overlay-container">  Hello World!</div>'),
+
   CSS: {
+    
+      'overlay_buttons': 'div.clappr-overlay-container{font-size:100px;color:#fff;background-color:rgba(134,134,134,.68);position:fixed;z-index:1;width:100%;height:100%;box-shadow:inset 0 0 10px 16px #000;text-shadow:0 0 6px #000}',
     
   }
 };
